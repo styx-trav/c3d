6059f1470ca8de1833153d3f3592de4b5500f7e7
lucas <lucas.tantin17@gmail.com>
2025-06-17 15:41:33 +0200

    sprite loaded but still problem sprite moving randomly

--- nao/bonuses/srcs/graphic/sprite.c
+++ nao/bonuses/srcs/graphic/sprite.c
@@ -28,31 +28,31 @@ int get_sprite(t_all *all)
 
 int load_sprite_frames(t_sprite *sprite, void *mlx)
 {
-    int w;
+	int w;
 	int h;
-    int i;;
-    char *paths[SPRITE_FRAMES] = 
+	int i;;
+	char *paths[SPRITE_FRAMES] = 
 	{
-        "bonuses/tex_sprites/shrek_wazowski_eyesclosed.xpm",
-        "bonuses/tex_sprites/shrek_wazowski.xpm",
-        "bonuses/tex_sprites/shrek_wazowski_mouseopen.xpm"
-    };
+		"bonuses/tex_sprites/shrek_wazowski_eyesclosed.xpm",
+		"bonuses/tex_sprites/shrek_wazowski.xpm",
+		"bonuses/tex_sprites/shrek_wazowski_mouseopen.xpm"
+	};
 
 	i = 0;
-    sprite->frame_index = 0;
-    sprite->last_update = get_current_time_ms();
-    while (i < SPRITE_FRAMES)
-    {
-        sprite->img[i].img = mlx_xpm_file_to_image(mlx, paths[i], &w, &h);
-        if (!sprite->img[i].img)
-            return 0;
-        sprite->img[i].addr = mlx_get_data_addr(sprite->img[i].img, &sprite->img[i].bpp,
-                                               &sprite->img[i].size_line, &sprite->img[i].endian);
-        sprite->img[i].width = w;
-        sprite->img[i].height = h;
-        i++;
-    }
-    return 1;
+	sprite->frame_index = 0;
+	sprite->last_update = get_current_time_ms();
+	while (i < SPRITE_FRAMES)
+	{
+		sprite->img[i].img = mlx_xpm_file_to_image(mlx, paths[i], &w, &h);
+		if (!sprite->img[i].img)
+			return 0;
+		sprite->img[i].addr = mlx_get_data_addr(sprite->img[i].img, &sprite->img[i].bpp,
+											   &sprite->img[i].size_line, &sprite->img[i].endian);
+		sprite->img[i].width = w;
+		sprite->img[i].height = h;
+		i++;
+	}
+	return 1;
 }
 
 
